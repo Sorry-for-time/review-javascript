c0e58409fee756ee7b9e5856a303d0867be7f490
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-12 22:18:22 +0800

    原型式继承测试

--- src/demos/3-9-object.js
+++ src/demos/3-9-object.js
@@ -28,3 +28,32 @@ console.log(sub2Instance); // SubType2 {name: 'Matt', job: 'Software Engineer'}
 
 // 正常使用父类原型上的方法
 sub2Instance.sayHi(); // hi
+
+console.log("-".repeat(30));
+// 原型式继承
+function createObject(o) {
+  function F() {}
+  F.prototype = o;
+  return new F();
+}
+const person = {
+  name: "Nicholas",
+  friends: ["Van", "Black"],
+};
+const otherPerson = createObject(person);
+console.log(otherPerson);
+console.log(otherPerson.name); // Nicholas
+
+console.log("-".repeat(30));
+console.log(person.friends); // ['Van', 'Black']
+const anotherPerson = Object.create(person, {
+  name: {
+    value: "Greg",
+  },
+});
+console.log(anotherPerson); // {name: 'Greg'}
+console.log(anotherPerson.friends); // ['van', 'Black']
+anotherPerson.friends.push("Smith");
+console.log(anotherPerson.friends); // ['van', 'Black', 'Smith']
+// 属性中包含的引用值始终会在相关对象之间共享
+console.log(person.friends); // ['van', 'Black', 'Smith']
