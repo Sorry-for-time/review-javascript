9e2ad891c254d233b9814e8cc90c4a3871d1b8c4
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-13 10:53:34 +0800

    移动文件到单独的文件夹

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import "./style.css";
 
-// import "./demos/1-iterator";
-// import "./demos/2-generator";
-// import "./demos/3-1-object";
-// import "./demos/3-2-object";
-// import "./demos/3-3-object";
-// import "./demos/3-4-object";
-// import "./demos/3-5-object";
-// import "./demos/3-6-object";
-// import "./demos/3-7-object"
-// import "./demos/3-8-object";
-// import "./demos/3-9-object";
-// import "./demos/3-10-object";
-// import "./demos/3-11-object";
-// import "./demos/3-12-object";
-// import "./demos/3-13-object";
-import "./demos/3-14-object";
+// import "./demos1/1-iterator";
+// import "./demos1/2-generator";
+// import "./demos1/3-1-object";
+// import "./demos1/3-2-object";
+// import "./demos1/3-3-object";
+// import "./demos1/3-4-object";
+// import "./demos1/3-5-object";
+// import "./demos1/3-6-object";
+// import "./demos1/3-7-object"
+// import "./demos1/3-8-object";
+// import "./demos1/3-9-object";
+// import "./demos1/3-10-object";
+// import "./demos1/3-11-object";
+// import "./demos1/3-12-object";
+// import "./demos1/3-13-object";
+import "./demos1/3-14-object";
