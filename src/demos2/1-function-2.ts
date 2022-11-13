447eca4b5f2831e776b2d4c725db7bd58a951ad0
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-13 13:02:18 +0800

    arguments 对象的一些行为

--- src/demos2/1-function-2.ts
+++ src/demos2/1-function-2.ts
@@ -45,3 +45,37 @@ console.log(numOperation(2, 2)); // 4
 // }
 // console.log(numOperation(3)); // 9
 // console.log(numOperation(2, 2)); // 4
+
+function doAdd(num1?: any, num2?: any) {
+  if (arguments.length === 1) {
+    return num1;
+    // 允许 arguments 对象和命名参数一起使用
+  } else if (arguments.length === 2) {
+    return arguments[0] + num2;
+  }
+}
+
+console.log(doAdd(12)); // 12
+console.log(doAdd(12, 12)); // 24
+console.log("-".repeat(40));
+
+// 非严格模式下才有效(否则并无同步的效果)
+function doOther(num1: any): void {
+  // arguments 对象的值会自动同步到对应的命名参数
+  // 且arguments 对象的长度是根据传入参数的个数
+  console.log("num1 origin:", num1);
+  arguments[0] = "CHANGE";
+  console.log("num1:", num1); // CHANGE
+  console.log("arguments[0]:", arguments[0]);
+}
+doOther("23");
+
+function doAnother(name = "A DEFAULT VALUE"): void {
+  console.log(name);
+  // 使用默认擦参数时, arguments 对象不会返回参数的默认值, 只反映传给函数的参数
+  // es5 严格模式下, 修改命名参数也不会影响 arguments 对象, 始终以调用函数时传人的值为准
+  console.log(arguments[0]);
+}
+doAnother();
+// A DEFAULT VALUE
+// undefined
