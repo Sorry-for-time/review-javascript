30651b81d06249c323934702cc73122448b75400
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-16 12:18:08 +0800

    利用函数序列化初始化行内脚本

--- src/demos4/web-worker-3.ts
+++ src/demos4/web-worker-3.ts
@@ -11,7 +11,26 @@ const workerBlob: Blob = new Blob([workerScript]);
 const workerScriptURL: string = URL.createObjectURL(workerBlob);
 
 // 基于对象 URL 创建专用工作者线程
-const worker = new Worker(workerScriptURL);
+const worker: Worker = new Worker(workerScriptURL);
 
 worker.postMessage("blob worker script");
 // blob worker script
+
+// =================================================================
+
+// 利用函数序列化方式初始化脚本
+function fibonacci(n: number): number {
+  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+// 函数的 toString() 方法返回函数代码的字符串
+const workerScript1 = `
+  self.postMessage(
+    (${fibonacci.toString()})(9)
+  );
+`;
+
+const worker1: Worker = new Worker(URL.createObjectURL(new Blob([workerScript1])));
+worker1.onmessage = ({ data }): void => {
+  console.log(data); // 34
+};
