f461834cadab6f0b0e04fca54436b98fd79cd90e
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-17 22:30:19 +0800

    encodeInto 方法测试

--- src/demos6/1-textEncoder.ts
+++ src/demos6/1-textEncoder.ts
@@ -25,4 +25,12 @@ window.addEventListener("load", (): void => {
   worker.addEventListener("message", (ev: MessageEvent<any>): void => {
     console.log(ev.data); // Uint8Array(17) [104, ..., 97, 121, buffer: ArrayBuffer(17), byteLength: 17, byteOffset: 0, length: 17, Symbol(Symbol.toStringTag): 'Uint8Array']
   });
+
+  // ==================================================================
+  const uint8Array: Uint8Array = new Uint8Array(120);
+  // encodeInto(str, uint8Array) 返回一个包含 read 和 written 属性的字典, 用于显示编码状态
+  const result: TextEncoderEncodeIntoResult = textEncoder.encodeInto(testStr, uint8Array);
+  // 如果定型数组空间不够, 就会立即停止编码操作
+  console.log(`读取成功了 ${result.read} 个字符`); // 读取成功了 17 个字符
+  console.log(`写入成功了 ${result.written} 个字符`); // 写入成功了 17 个字符
 });
