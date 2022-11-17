b776a3a5852d6df816404a7d18225655e3fd64aa
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-17 23:31:09 +0800

    流解码器测试2

--- src/demos6/3-textDecoderStream.ts
+++ src/demos6/3-textDecoderStream.ts
@@ -2,7 +2,10 @@ export {};
 
 async function* chars() {
   // 每个块必须是一个定型数组
-  const encodeText: Array<Uint8Array> = [102, 111, 111].map((e: number): Uint8Array => Uint8Array.of(e));
+  //   const encodeText: Array<Uint8Array> = [102, 111, 111].map((e: number): Uint8Array => Uint8Array.of(e));
+  // 😊 的十进制编码如下
+  const encodeText: Array<Uint8Array> = [240, 159, 152, 138].map((e: number): Uint8Array => Uint8Array.of(e));
+
   for (const char of encodeText) {
     yield await new Promise((resolve) => setTimeout(resolve, 1000, char));
   }
@@ -30,15 +33,13 @@ window.addEventListener("load", (): void => {
       if (done) {
         break;
       } else {
-        console.log(value);
+        // 文本解码器流能识别可能分散子啊不同块上的代理对, 解码器流会保持片段直到
+        // 取得完整的字符
+        console.log(value); // 😊(这里只会输出一次, 因为流解码器在解码流输出字符之前会等待传入 4 个块)
         str += value;
       }
     }
-    // f
-    // o
-    // o
 
-    console.log(str);
-    // foo
+    console.log(str); // 😊
   })();
 });
