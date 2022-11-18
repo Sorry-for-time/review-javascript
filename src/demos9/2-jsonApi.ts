25a7e60d14bf8ed5b7c723dae6f560f9b2f2d6d5
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-18 18:25:09 +0800

    JSON.stringify API 简单测试

---
+++ src/demos9/2-jsonApi.ts
@@ -0,0 +1,69 @@
+export {};
+
+const object = {
+  name: "counter",
+  value: 0,
+  increment(): void {
+    this.value++;
+  },
+  tags: [-2, -1, 0, 1, 2],
+};
+
+// 默认方式
+const str1: string = JSON.stringify(object);
+console.log(str1);
+// {"name":"counter","value":0,"tags":[-2,-1,0,1,2]}
+
+// 如果第二个参数是数组, 那返回的字符串只会包含指定的 key 的属性
+const str2: string = JSON.stringify(object, ["name", "value"]);
+console.log(str2);
+// {"name":"counter","value":0}
+
+// 如果第二个参数是函数, 那么可以根据 key 对 需要返回的 value 进行相应的处理
+const str3: string = JSON.stringify(
+  object,
+  // 返回的值就是相应 key 应该包含的结果
+  (key: string, value: any): any => {
+    // 如果是函数类型, 那么就调用 toString() 进行序列化再截取字符串
+    if (typeof (<any>object)[key] === "function") {
+      const result: string = ((<any>object)[key] as Function).toString();
+      // 去除所有的空格和换行, 方便进行还原解析
+      return result.substring(key.length, result.length).replaceAll(/\s*/g, "");
+    }
+    return value;
+  }
+);
+console.log(str3);
+// {"name":"counter","value":0,"increment":"(){this.value++;}","tags":[-2,-1,0,1,2]}
+
+// 第三个参数可以用于指定缩进, 默认为0(即不缩进), 最大缩进值为 10, 超出部分会设置截断
+// 如果指定了缩进,
+const str4: string = JSON.stringify(
+  object,
+  // 返回的值就是相应 key 应该包含的结果
+  (key: string, value: any): any => {
+    // 如果是函数类型, 那么就调用 toString() 进行序列化再截取字符串
+    if (typeof (<any>object)[key] === "function") {
+      const result: string = ((<any>object)[key] as Function).toString();
+      // 去除所有的空格和换行, 方便进行还原解析
+      return result.substring(key.length, result.length).replaceAll(/\s*/g, "");
+    }
+    return value;
+  },
+  4
+);
+console.log(str4);
+/*
+{
+    "name": "counter",
+    "value": 0,
+    "increment": "(){this.value++;}",
+    "tags": [
+        -2,
+        -1,
+        0,
+        1,
+        2
+    ]
+}
+*/
--- src/main.ts
+++ src/main.ts
@@ -58,4 +58,5 @@ import "./style.css";
 // import "./demos8/2-writableStream";
 // import "./demos8/3-transformStream";
 // import "./demos8/4-pipedStream";
-import "./demos9/1-highResolutionApi";
+// import "./demos9/1-highResolutionApi";
+import "./demos9/2-jsonApi";
