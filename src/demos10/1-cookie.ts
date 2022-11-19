467c6beaa67bf4f8297361ab05b1f854357b0a87
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-19 23:26:15 +0800

    WebStorage 存储触发事件回顾

--- src/demos10/1-cookie.ts
+++ src/demos10/1-cookie.ts
@@ -6,3 +6,17 @@ document.cookie =
   encodeURIComponent("user") + "=" + encodeURIComponent("Nicholas");
 const cookies: string = document.cookie;
 console.log(cookies); // name=just_test; user=Nicholas
+console.log("-".repeat(40));
+
+// 存储事件监听测试
+window.addEventListener("storage", (event: StorageEvent): void => {
+  console.log(event);
+});
+
+// 注: 所有现代浏览器在实现存储写入时都使用了同步阻塞方式, 因此数据会立即提交到存储(不考虑老 IE)
+window.localStorage.setItem("key1", "value1");
+window.localStorage.setItem("key1", "value1111");
+console.log(window.localStorage.getItem("key1")); // value111
+window.localStorage.removeItem("key1");
+window.localStorage.setItem("key111", "value111");
+window.localStorage.clear();
