1cd98d7a365ddc17377c243f5e13a13b42dfe9d2
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-22 23:11:12 +0800

    基本属性

--- src/demos11/1-Node-type.ts
+++ src/demos11/1-Node-type.ts
@@ -19,3 +19,19 @@ console.log("before", nodes);
 document.body.appendChild(document.createElement("span"));
 console.log("after", nodes);
 // 每个节点都有一个 childNodes 属性, 其中包含一个 NodeList 实例, 其独特在于其实是一个 dom 结构的查询, 因此 dom 结构的变化会自动地在 NodeList 上反映出来 --> 即为一个活动对象, 而不是第一次获取时所获得内容的快照
+
+console.log("-".repeat(40));
+// 将类数组转化为数组
+const timeNodes: Array<ChildNode> = Array.from(nodes);
+timeNodes.forEach((v) => {
+  console.log(v);
+});
+
+// 判断一个节点是否包含一个或多个子节点
+console.log(document.body.querySelector("#app")!.hasChildNodes());
+
+// 可以通过 nextSibling 访问当前节点相邻的下一个节点
+// 可以通过 previousSibling 访问当前节点相邻的上一个节点
+
+// ownerDocument 属性是一个指向代表整个文档的节点的指针
+console.log(timeNodes[0].ownerDocument);
