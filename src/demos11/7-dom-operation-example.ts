c9a12949c838ee679062fbcdd58d00f0cb5b8c01
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-24 18:18:31 +0800

    实现一个位置不那么准确的预览

--- src/demos11/7-dom-operation-example.ts
+++ src/demos11/7-dom-operation-example.ts
@@ -3,12 +3,18 @@ export {};
 window.addEventListener("load", (): void => {
   const mainPic: HTMLDivElement = document.querySelector(".main-pic")!;
   const cursor: HTMLSpanElement = document.querySelector(".cursor")!;
+  const zoomPreview: HTMLDivElement = document.querySelector(".zoom-preview")!;
+
   mainPic.addEventListener("mouseover", (): void => {
     cursor.classList.remove("hidden");
+    zoomPreview.classList.remove("hidden");
   });
   mainPic.addEventListener("mouseleave", (): void => {
     if (!cursor.classList.contains("hidden")) {
       cursor.classList.add("hidden");
+    }
+    if (!zoomPreview.classList.contains("hidden")) {
+      zoomPreview.classList.add("hidden");
     }
   });
   cursor.style.willChange = "transform";
@@ -40,6 +46,8 @@ window.addEventListener("load", (): void => {
     }
     cursor.style.setProperty("--x", `${left}px`);
     cursor.style.setProperty("--y", `${top}px`);
+
+    zoomPreview.style.backgroundPosition = `${left / 2.02}% ${top / 2.02}%`;
   }
 
   mainPic.addEventListener("mousemove", handler);
