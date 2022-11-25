f4af2df3186be5c35438f1f05bf64dea79987c33
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-26 00:37:16 +0800

    移动端事件简单测试

--- src/demos11/10-event.ts
+++ src/demos11/10-event.ts
@@ -22,6 +22,15 @@ window.addEventListener("load", (): void => {
   input.addEventListener("input", (): void => {
     preview.textContent = input.value;
   });
+
+  const showPosition: HTMLDivElement = document.querySelector(".position")!;
+  window.addEventListener("touchstart", (): void => {
+    window.addEventListener("touchmove", (ev: TouchEvent): void => {
+      showPosition.textContent = `x: ${ev.touches[0].pageX.toFixed(
+        2
+      )} | y: ${ev.touches[0].pageY.toFixed(2)}`;
+    });
+  });
 });
 
 // window.addEventListener("beforeunload", (ev: BeforeUnloadEvent) => {
