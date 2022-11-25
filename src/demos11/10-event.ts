c5e0a6034bc850c41cb898e0d36c353dee93e1a5
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-25 22:39:30 +0800

    键盘事件测试

--- src/demos11/10-event.ts
+++ src/demos11/10-event.ts
@@ -1,1 +1,25 @@
 export {};
+import { throttle } from "lodash-es";
+import "../scss/input.scss";
+
+window.addEventListener("load", (): void => {
+  const input: HTMLInputElement = document.querySelector("input")!;
+  const preview: HTMLDivElement = document.querySelector(".preview-text")!;
+
+  const handler = throttle((ev: KeyboardEvent): void => {
+    console.log(ev.code);
+    console.log(ev.location);
+
+    if (ev.code === "Enter") {
+      input.blur();
+    }
+    if (ev.code === "Escape") {
+      input.blur();
+    }
+  }, 200);
+
+  input.addEventListener("keydown", handler);
+  input.addEventListener("input", (): void => {
+    preview.textContent = input.value;
+  });
+});
