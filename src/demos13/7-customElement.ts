8aaeaba90548e8b2073ace09096efdb559a0dc9a
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-26 18:10:08 +0800

    使用自定义元素生命周期方法

--- src/demos13/7-customElement.ts
+++ src/demos13/7-customElement.ts
@@ -1,6 +1,8 @@
 export {};
 
 class CustomOne extends HTMLElement {
+  private innerP: HTMLParagraphElement | null = null;
+
   constructor() {
     super();
     // 引用 web 组件节点
@@ -27,12 +29,37 @@ class CustomOne extends HTMLElement {
       <p><slot></slot></p>
       <div><slot name="extra"></slot></div>
     `;
-    // 在内部添加监听器
-    this.shadowRoot
-      ?.querySelector("p")!
-      .addEventListener("click", (ev: MouseEvent): void => {
-        console.log(ev.target);
-      });
+  }
+
+  private clickHandler(ev: MouseEvent): void {
+    console.log(ev.clientX, ev.clientY);
+  }
+
+  /**
+   * 当元素渲染到真实 dom 上时触发
+   */
+  public connectedCallback(): void {
+    console.log("connected");
+    console.log("binding listener");
+    this.innerP = this.shadowRoot?.querySelector("p")!;
+    this.innerP && this.innerP.addEventListener("click", this.clickHandler);
+  }
+
+  /**
+   * 在元素从真实 dom 上卸载时触发
+   */
+  public disconnectedCallback(): void {
+    console.log("disconnected");
+    console.log("remove listener");
+    this.innerP && this.innerP.removeEventListener("click", this.clickHandler);
+  }
+
+  public attributeChangedCallback(): void {
+    console.log("attributeChangedCallback");
+  }
+
+  public adoptedCallBack(): void {
+    console.log("adoptedCallBack");
   }
 }
 
@@ -40,8 +67,7 @@ window.addEventListener("load", (): void => {
   // 创建自定义元素
   customElements.define("custom-one", CustomOne);
   const app: HTMLDivElement = document.querySelector("#app")!;
-  const customOne: CustomOne = document.createElement("custom-one");
-
+  const customOne: CustomOne = new CustomOne();
   customOne.textContent = "INSIDE IN A CUSTOM ELEMENT";
   const h4: HTMLHeadingElement = document.createElement("h4");
   h4.setAttribute("slot", "extra");
@@ -49,4 +75,7 @@ window.addEventListener("load", (): void => {
   customOne.appendChild(h4);
 
   app.appendChild(customOne);
+  setTimeout(() => {
+    app.removeChild(customOne);
+  }, 4000);
 });
