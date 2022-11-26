670bfd03fc937e37e6feba8b4a9547dde3af5c37
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-11-26 17:07:34 +0800

    is属性和extends选项

--- src/demos13/6-customElement.ts
+++ src/demos13/6-customElement.ts
@@ -7,14 +7,27 @@ class CustomTag extends HTMLElement {
   }
 }
 
+class CustomTagSecond extends HTMLDivElement {
+  constructor() {
+    super();
+    console.log("created");
+  }
+}
+
 window.addEventListener("load", (): void => {
   // 创建自定义元素
   customElements.define("custom-tag", CustomTag);
+
+  customElements.define("custom-tag-second", CustomTagSecond, {
+    extends: "div",
+  });
 
   const app: HTMLDivElement = document.querySelector("#app")!;
   app.innerHTML = `
     <custom-tag>1</custom-tag>
     <custom-tag>2</custom-tag>
     <custom-tag>3</custom-tag>
+    <div is="custom-tag-second">the div</div>
   `;
+  // 如果自定义元素继承了一个元素类, 那么可以使用 is 属性和 extends 选项将该标签指定为自定义元素的实例
 });
