091422d738fdddf480b7f6536721e2cda0d76c81
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-12-11 12:28:22 +0800

    添加 防抖以及节流处理的装饰器

---
+++ src/utils/performanceUtil.ts
@@ -0,0 +1,71 @@
+/**
+ * 防抖装饰器
+ * @param wait 间隔等待时间
+ * @param startImmediate 首次是否立即执行
+ * @returns 操作防抖实例
+ */
+function useDebounce(
+  wait: number = 100,
+  startImmediate: boolean = true
+): MethodDecorator {
+  return <T>(
+    target: Object,
+    _propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<T>
+  ): TypedPropertyDescriptor<T> | void => {
+    let timer: number;
+    const original: Function = descriptor.value as Function;
+    (descriptor.value as any) = function (...params: Array<any>): void {
+      if (startImmediate) {
+        startImmediate = false;
+        timer = window.setTimeout((): void => {
+          original.call(target, ...params);
+        });
+      } else {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = window.setTimeout((): void => {
+          original.call(target, ...params);
+        }, wait);
+      }
+    };
+  };
+}
+
+/**
+ * 节流装饰器
+ * @param wait 间隔等待时间
+ * @param startImmediate 首次是否立即执行
+ * @returns 节流防抖实例
+ */
+function useThrottle(
+  wait: number = 100,
+  startImmediate: boolean = true
+): MethodDecorator {
+  return <T>(
+    target: Object,
+    _propertyKey: string | symbol,
+    descriptor: TypedPropertyDescriptor<T>
+  ): TypedPropertyDescriptor<T> | void => {
+    let locker: boolean = false;
+    const original: Function = descriptor.value as Function;
+    (descriptor.value as any) = (...params: Array<any>) => {
+      if (startImmediate) {
+        startImmediate = false;
+        original.call(target, ...params);
+      } else {
+        if (locker) {
+          return;
+        }
+        locker = true;
+        setTimeout((): void => {
+          original.call(target, ...params);
+          locker = false;
+        }, wait);
+      }
+    };
+  };
+}
+
+export { useDebounce, useThrottle };
