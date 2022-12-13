3f854e4a766a9f195129c442de17428d45ca7396
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-12-13 17:35:25 +0800

    设置选择器参数

--- src/demos14/3-test-custom-decorator.ts
+++ src/demos14/3-test-custom-decorator.ts
@@ -2,14 +2,19 @@ import { useThrottle, effectA, effectB } from "../utils/performanceUtil";
 
 const drawingArea: HTMLElement = document.querySelector("#axis")!;
 export class DecoratorTest {
-  private box: HTMLButtonElement;
+  private box: HTMLElement;
 
-  constructor() {
-    this.box = document.querySelector("#test-decorator")!;
+  constructor(selector: string) {
+    const res: HTMLButtonElement = document.querySelector(selector)!;
+    if (res) {
+      this.box = res;
+    } else {
+      throw new Error("can not get the dom target");
+    }
   }
 
   public bindListener(): void {
-    this.box.addEventListener("mousemove", this.moveHandler);
+    this.box!.addEventListener("mousemove", this.moveHandler);
   }
 
   public draw(x: number, y: number): void {
@@ -19,13 +24,13 @@ export class DecoratorTest {
   // 装饰器执行顺序: effectB -> effectB -> useThrottle
   @useThrottle(200, true)
   @effectA
-  @effectB() // 实际上会先执行 effectB, 然后才是 effectB, 类似于 f(g(x)), 会先求 g(x), 再进行外层的计算
+  @effectB() // 实际上会先执行 effectB, 然后才是 effectB, 类似于 f(g(x)), 会先求 g(x), 然后才是 f(x)
   private moveHandler(ev: MouseEvent): void {
     this.draw(ev.clientX, ev.clientY);
   }
 }
 
 window.addEventListener("load", (): void => {
-  const instance = new DecoratorTest();
+  const instance = new DecoratorTest("#test-decorator");
   instance.bindListener();
 });
